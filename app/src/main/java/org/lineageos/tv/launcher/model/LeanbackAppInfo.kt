5bc4c66352eb4f241f1baf73336c20f9707f881b
Timi <timi.rautamaki@gmail.com>
2024-12-21 21:32:49 +0200

    Catapult: Query all apps
    Instead of only querying for leanback launcher activities, query all
    activities to display in the launcher.
    
    * Renamed AppInfo to LeanbackAppInfo
    * Created new type AppInfo to query non-leanback launch intent
    
    Change-Id: I026b3c8ac951a46c030224256f919cf597a73ced

---
+++ app/src/main/java/org/lineageos/tv/launcher/model/LeanbackAppInfo.kt
@@ -0,0 +1,48 @@
+/*
+ * SPDX-FileCopyrightText: 2024 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.tv.launcher.model
+
+import android.content.Context
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import android.content.pm.ResolveInfo
+import android.graphics.drawable.Drawable
+import org.lineageos.tv.launcher.utils.AppManager.uninstallable
+
+open class LeanbackAppInfo : Launchable {
+    var banner: Drawable?
+    protected val packageManager: PackageManager = context.packageManager
+    private val applicationInfo: ApplicationInfo
+
+    constructor(resolveInfo: ResolveInfo, context: Context) : super(
+        resolveInfo.loadLabel(context.packageManager).toString(),
+        resolveInfo.activityInfo.packageName,
+        resolveInfo.loadIcon(context.packageManager),
+        context
+    ) {
+        applicationInfo = resolveInfo.activityInfo.applicationInfo
+        banner = resolveInfo.activityInfo.loadBanner(packageManager)
+        if (banner == null) {
+            banner = resolveInfo.activityInfo.applicationInfo.loadBanner(packageManager)
+        }
+    }
+
+    constructor(app: ApplicationInfo, context: Context) : super(
+        app.loadLabel(context.packageManager).toString(),
+        app.packageName,
+        app.loadIcon(context.packageManager),
+        context
+    ) {
+        applicationInfo = app
+        banner = app.loadBanner(packageManager)
+    }
+
+    override fun setIntent() = packageManager.getLeanbackLaunchIntentForPackage(packageName)
+
+    fun isUninstallable(): Boolean {
+        return uninstallable(applicationInfo, context)
+    }
+}
