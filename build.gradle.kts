ff6c084ba63b6386c9709e380289a7fcd2515158
Luca Stefani <luca.stefani.ge1@gmail.com>
2025-01-10 11:22:18 +0100

    Catapult: Migrate generatebp to plugin declaration
    Also remove nodeps targets
    
    Change-Id: I3b5df33f1fdd4d7af046b427c098ce1a51209096

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2024 The LineageOS Project
+ * SPDX-FileCopyrightText: 2024-2025 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -8,6 +8,7 @@ plugins {
     id("com.android.application") version "8.7.3" apply false
     id("com.android.library") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("org.lineageos.generatebp") version "+" apply false
 }
 
 tasks.register<Delete>("clean").configure {
