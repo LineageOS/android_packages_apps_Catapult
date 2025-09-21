007c15f5057405dbdac8bee6a15fd7907b30f610
Timi <timi.rautamaki@gmail.com>
2025-09-21 18:40:16 +0300

    Catapult: Update dependencies
    Roughly match what AOSP uses
    
    Change-Id: Ifee6b7cff05dfc2006cfe7956b5c846003590c24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,15 +59,15 @@ android {
 dependencies {
     compileOnly(fileTree(mapOf("dir" to "../libs", "include" to listOf("*.jar"))))
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.activity:activity-ktx:1.7.2")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.leanback:leanback:1.2.0-alpha04")
+    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.activity:activity-ktx:1.11.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
+    implementation("androidx.leanback:leanback:1.2.0")
     implementation("androidx.preference:preference:1.2.1")
-    implementation("androidx.tvprovider:tvprovider:1.0.0")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("io.coil-kt:coil:2.6.0")
+    implementation("androidx.tvprovider:tvprovider:1.1.0")
+    implementation("com.google.android.material:material:1.13.0")
+    implementation("io.coil-kt:coil:2.7.0")
 }
 
 configure<GenerateBpPluginExtension> {
