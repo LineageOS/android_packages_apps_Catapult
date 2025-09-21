4db54b9baf686bef8704a8b555112519d545a4f9
Timi <timi.rautamaki@gmail.com>
2025-09-21 18:10:22 +0300

    Catapult: Add blurred watch next art as background
    It looks little bit better on non 16:9 art.
    
    Change-Id: I2cc26890c485027a6a5fb597b87367519e34c706

--- app/src/main/java/org/lineageos/tv/launcher/view/WatchNextCard.kt
+++ app/src/main/java/org/lineageos/tv/launcher/view/WatchNextCard.kt
@@ -7,13 +7,22 @@ package org.lineageos.tv.launcher.view
 
 import android.animation.AnimatorInflater
 import android.content.Context
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
+import android.graphics.RenderEffect
+import android.graphics.Shader
+import android.media.tv.TvContract
 import android.util.AttributeSet
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 import androidx.tvprovider.media.tv.BasePreviewProgram
+import androidx.tvprovider.media.tv.TvContractCompat
 import coil.load
+import coil.size.Scale
+import coil.transform.RoundedCornersTransformation
 import com.google.android.material.progressindicator.LinearProgressIndicator
 import org.lineageos.tv.launcher.R
 import org.lineageos.tv.launcher.ext.getAttributeResourceId
@@ -22,7 +31,9 @@ class WatchNextCard @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : Card(context, attrs, defStyleAttr) {
     // Views
+    private val bannerBackgroundView: ImageView by lazy { findViewById(R.id.app_banner_background)!! }
     private val bannerView: ImageView by lazy { findViewById(R.id.app_banner)!! }
+    private val smallBannerView: ImageView by lazy { findViewById(R.id.app_banner_small)!! }
     private val title: TextView by lazy { findViewById(R.id.title)!! }
     private val progressView: LinearProgressIndicator by lazy { findViewById(R.id.watch_progress)!! }
 
@@ -48,9 +59,12 @@ class WatchNextCard @JvmOverloads constructor(
 
     @Suppress("RestrictedApi")
     fun setInfo(info: BasePreviewProgram) {
+        bannerView.isVisible = false
+        smallBannerView.isVisible = false
+        progressView.isVisible = false
+
         title.isInvisible = true
         label = info.title
-        bannerView.isVisible = true
         launchIntent = info.intent
         title.text = info.title
 
@@ -63,6 +77,47 @@ class WatchNextCard @JvmOverloads constructor(
             }
         }
 
+        // Other than 16:9, use blurred background and smaller art
+        if (info.posterArtAspectRatio != TvContractCompat.PreviewPrograms.ASPECT_RATIO_16_9) {
+            bannerView.isVisible = false
+            smallBannerView.isVisible = true
+            bannerBackgroundView.load(info.posterArtUri) {
+                placeholder(
+                    context.getAttributeResourceId(
+                        com.google.android.material.R.attr.colorSecondaryContainer
+                    )
+                )
+                crossfade(500)
+                scale(Scale.FILL)
+                listener(onSuccess = { _, _ ->
+                    bannerBackgroundView.setRenderEffect(
+                        RenderEffect.createBlurEffect(
+                            25f, 25f, Shader.TileMode.CLAMP
+                        )
+                    )
+
+                    bannerBackgroundView.colorFilter = PorterDuffColorFilter(
+                        ContextCompat.getColor(context, R.color.watchNextCardFilter),
+                        PorterDuff.Mode.SRC_ATOP
+                    )}
+                )
+            }
+
+            smallBannerView.load(info.posterArtUri) {
+                placeholder(
+                    context.getAttributeResourceId(
+                        com.google.android.material.R.attr.colorSecondaryContainer
+                    )
+                )
+                crossfade(500)
+                transformations(RoundedCornersTransformation(5F))
+            }
+
+            return
+        }
+
+        bannerView.isVisible = true
+        smallBannerView.isVisible = false
         bannerView.load(info.posterArtUri) {
             placeholder(
                 context.getAttributeResourceId(
