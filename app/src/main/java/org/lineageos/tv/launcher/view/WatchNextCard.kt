3283593ed14100c4563724a48b62a59e260c1ccc
Timi <timi.rautamaki@gmail.com>
2025-09-21 18:10:22 +0300

    Catapult: Add blurred watch next art as background
    It looks little bit better on non 16:9 art.
    
    Change-Id: I2cc26890c485027a6a5fb597b87367519e34c706

--- app/src/main/java/org/lineageos/tv/launcher/view/WatchNextCard.kt
+++ app/src/main/java/org/lineageos/tv/launcher/view/WatchNextCard.kt
@@ -7,6 +7,8 @@ package org.lineageos.tv.launcher.view
 
 import android.animation.AnimatorInflater
 import android.content.Context
+import android.graphics.RenderEffect
+import android.graphics.Shader
 import android.util.AttributeSet
 import android.widget.ImageView
 import android.widget.TextView
@@ -14,6 +16,7 @@ import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 import androidx.tvprovider.media.tv.BasePreviewProgram
 import coil.load
+import coil.size.Scale
 import com.google.android.material.progressindicator.LinearProgressIndicator
 import org.lineageos.tv.launcher.R
 import org.lineageos.tv.launcher.ext.getAttributeResourceId
@@ -22,6 +25,7 @@ class WatchNextCard @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : Card(context, attrs, defStyleAttr) {
     // Views
+    private val bannerBackgroundView: ImageView by lazy { findViewById(R.id.app_banner_background)!! }
     private val bannerView: ImageView by lazy { findViewById(R.id.app_banner)!! }
     private val title: TextView by lazy { findViewById(R.id.title)!! }
     private val progressView: LinearProgressIndicator by lazy { findViewById(R.id.watch_progress)!! }
@@ -61,6 +65,25 @@ class WatchNextCard @JvmOverloads constructor(
                 progressView.progress = percentWatched
                 progressView.isVisible = true
             }
+        }
+
+        bannerBackgroundView.load(info.posterArtUri) {
+            placeholder(
+                context.getAttributeResourceId(
+                    com.google.android.material.R.attr.colorSecondaryContainer
+                )
+            )
+            crossfade(500)
+            scale(Scale.FILL)
+            listener(
+                onSuccess = { _, _ ->
+                    bannerBackgroundView.setRenderEffect(
+                        RenderEffect.createBlurEffect(
+                            25f, 25f, Shader.TileMode.CLAMP
+                        )
+                    )
+                }
+            )
         }
 
         bannerView.load(info.posterArtUri) {
