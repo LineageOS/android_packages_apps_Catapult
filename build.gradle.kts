7f21e1ccbeaca955f5b1c96168685125ebbaabde
Timi <timi.rautamaki@gmail.com>
2025-09-21 18:40:16 +0300

    Catapult: Update dependencies
    Roughly match what AOSP uses
    
    Change-Id: Ifee6b7cff05dfc2006cfe7956b5c846003590c24

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,9 @@
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("com.android.library") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("com.android.application") version "8.11.2" apply false
+    id("com.android.library") version "8.11.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
     id("org.lineageos.generatebp") version "+" apply false
 }
 
