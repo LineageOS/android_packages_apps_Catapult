122b67d07d55f94e9f8b9b92add482a4cd6bcc1d
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-09-24 20:39:02 +0200

    Catapult: Update SDK to 36
    Change-Id: I6cc474e00362348b4fb5c97c2648bebda5a4c0e6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,12 +14,12 @@ plugins {
 
 android {
     namespace = "org.lineageos.tv.launcher"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "org.lineageos.tv.launcher"
         minSdk = 31
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
