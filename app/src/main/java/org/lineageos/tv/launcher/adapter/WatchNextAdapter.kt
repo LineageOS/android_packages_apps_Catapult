bdd6e45523affd8d6a690c1dc3cdc08955ca79ad
Timi <timi.rautamaki@gmail.com>
2026-08-09 14:34:34 +0300

    Catapult: Update WatchNextAdapter sorting
    Sort watch next items first by type, then by series. This way one
    series continue watching and watch next cards stay next to each other.
    
    Change-Id: I6da1fc66b2d9cd0fb834fad39920efe0dd4b0e3e

--- app/src/main/java/org/lineageos/tv/launcher/adapter/WatchNextAdapter.kt
+++ app/src/main/java/org/lineageos/tv/launcher/adapter/WatchNextAdapter.kt
@@ -5,15 +5,24 @@
 
 package org.lineageos.tv.launcher.adapter
 
+import android.annotation.SuppressLint
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import androidx.tvprovider.media.tv.TvContractCompat
 import androidx.tvprovider.media.tv.WatchNextProgram
 import org.lineageos.tv.launcher.view.WatchNextCard
 
 class WatchNextAdapter : ListAdapter<WatchNextProgram, WatchNextAdapter.ViewHolder>(DIFF_UTIL) {
+    override fun submitList(list: List<WatchNextProgram>?) {
+        super.submitList(list?.groupedBySeries())
+    }
+
+    override fun submitList(list: List<WatchNextProgram>?, commitCallback: Runnable?) {
+        super.submitList(list?.groupedBySeries(), commitCallback)
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(
         WatchNextCard(parent.context).apply {
             layoutParams = ViewGroup.LayoutParams(
@@ -55,5 +64,29 @@ class WatchNextAdapter : ListAdapter<WatchNextProgram, WatchNextAdapter.ViewHold
                 newItem: WatchNextProgram
             ) = oldItem.hasAnyUpdatedValues(newItem)
         }
+
+        private fun WatchNextProgram.typePriority(): Int = when (watchNextType) {
+            TvContractCompat.WatchNextPrograms.WATCH_NEXT_TYPE_CONTINUE -> 0
+            TvContractCompat.WatchNextPrograms.WATCH_NEXT_TYPE_NEXT -> 1
+            TvContractCompat.WatchNextPrograms.WATCH_NEXT_TYPE_NEW -> 2
+            TvContractCompat.WatchNextPrograms.WATCH_NEXT_TYPE_WATCHLIST -> 3
+            else -> 4
+        }
+
+        private val watchNextComparator = compareBy<WatchNextProgram>(
+            { it.typePriority() },
+            { -it.lastEngagementTimeUtcMillis }
+        )
+
+        // Group by series after WatchNextProgram.typePriority, so that one series cards stay
+        // next to each other. E.g. Series1 continue watching, then Series1 next episode.
+        private fun List<WatchNextProgram>.groupedBySeries(): List<WatchNextProgram> =
+            sortedWith(watchNextComparator)
+                .groupBy { it.seriesKey() }
+                .values
+                .flatten()
+
+        @SuppressLint("RestrictedApi")
+        private fun WatchNextProgram.seriesKey(): String = "$packageName::${title ?: "id-$id"}"
     }
 }
