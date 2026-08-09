8fdf5dc44f87fedd9e8e815f88734e0cdd8a9b77
Timi <timi.rautamaki@gmail.com>
2026-08-09 14:15:56 +0300

    Catapult: Update watch next card title
    Change-Id: I950fcacd716a20fd57d1c304fea88ae261e7ad82

---
+++ app/src/main/java/org/lineageos/tv/launcher/ext/BasePreviewProgram.kt
@@ -0,0 +1,31 @@
+package org.lineageos.tv.launcher.ext
+
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.tvprovider.media.tv.BasePreviewProgram
+import org.lineageos.tv.launcher.R
+
+@SuppressLint("RestrictedApi")
+fun BasePreviewProgram.displayTitle(context: Context): String {
+    val season = seasonNumber
+    val episode = episodeNumber
+
+    if (season.isNullOrBlank() && episode.isNullOrBlank()) {
+        return title.orEmpty()
+    }
+
+    val tag = buildString {
+        if (!season.isNullOrBlank()) {
+            append(context.getString(R.string.program_season, season))
+        }
+        if (!episode.isNullOrBlank()) {
+            append(context.getString(R.string.program_episode, episode))
+        }
+    }
+
+    return context.getString(
+        R.string.program_title_with_episode_info,
+        title.orEmpty(),
+        tag
+    )
+}
